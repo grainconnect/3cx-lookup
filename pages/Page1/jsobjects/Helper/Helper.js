24a10610b8becad8edf411c1de63186d53bfa259
samuel.wilson <samuel.wilson@grainconnect.com>
2024-09-05 15:44:39 +0000

    temp fix for issues with + in call name

--- pages/Page1/jsobjects/Helper/Helper.js
+++ pages/Page1/jsobjects/Helper/Helper.js
@@ -6,8 +6,8 @@ export default {
 	async formatSearch(){
 		let number = phone_number.text.replace("+44", "0").replace(" ", "")
 		if(number.slice(0,2) == "44"){
-    number = number.replace("44", "0" )
-  }
+			number = number.replace("44", "0" )
+		}
 		let date_init = moment(start_date.formattedDate).startOf('day').toISOString()
 		let date_end = moment(end_date.formattedDate).endOf('day').toISOString()
 		await getCallList.run({
@@ -18,16 +18,20 @@ export default {
 	},
 	async downloadFile(){
 		var zip = JSZip()
-		for( const file of Call_Information.selectedRows){
-			await GetAudioFile.run({"filename" : file.filename})
-			const data = GetAudioFile.data.fileData
 
-
-			zip.folder("calls").file(file.filename, data, {base64 : true})
+		for (const file of Call_Information.selectedRows){
+			console.log(file.filename)
 		}
 
-
-
+		for(const file of Call_Information.selectedRows){
+			let filename = Helper.removePluses(file.filename)
+			
+			console.log("downloading " + filename)
+			await GetAudioFile.run({"filename" : filename})
+			console.log("downloaded " + filename)
+			const data = GetAudioFile.data.fileData
+			await zip.folder("calls").file(file.filename, data, {base64 : true})
+		}
 		zip.generateAsync({type:"blob"})
 			.then(function (blob) {
 
@@ -38,5 +42,12 @@ export default {
 
 
 		//download("data:audio/wav;base64,"+GetAudioFile.data.fileData, file.filename, 'wav')
+	},
+	removePluses(rawString){
+
+		
+		rawString = "1029/[07901090664%3AMAIN NUMBER%3AOther+Calls+(16%2F11%2F2023)]_+447901090664-1029_20240724080634(45146).wav"
+		const newString = rawString.replace(/([+](?!44))/g," ")	
+		return newString
 	}
 }
